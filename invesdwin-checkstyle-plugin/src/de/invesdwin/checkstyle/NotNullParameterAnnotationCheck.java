f6c1cf92dc81a70aa179c9ac90ec5e189cc7155b
subes <gsubes@gmail.com>
2015-10-09 13:26:40 +0200

    compile fix

--- invesdwin-checkstyle-plugin/src/de/invesdwin/checkstyle/NotNullParameterAnnotationCheck.java
+++ invesdwin-checkstyle-plugin/src/de/invesdwin/checkstyle/NotNullParameterAnnotationCheck.java
@@ -1,6 +1,5 @@
 package de.invesdwin.checkstyle;
 
-import com.puppycrawl.tools.checkstyle.AnnotationUtility;
 import com.puppycrawl.tools.checkstyle.api.Check;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
