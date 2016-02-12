cff6247eff8fe1f723a7a5c4453b8d7dec13b683
subes <gsubes@gmail.com>
2016-02-12 21:20:38 +0100

    rule init fix

--- invesdwin-checkstyle-plugin/src/de/invesdwin/checkstyle/StackOverflowErrorIgnoringRegexpCheck.java
+++ invesdwin-checkstyle-plugin/src/de/invesdwin/checkstyle/StackOverflowErrorIgnoringRegexpCheck.java
@@ -1,17 +1,33 @@
 package de.invesdwin.checkstyle;
 
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
-import com.puppycrawl.tools.checkstyle.api.ExtendableRegexpCheck;
+import com.puppycrawl.tools.checkstyle.checks.regexp.RegexpCheck;
 
-public class StackOverflowErrorIgnoringRegexpCheck extends ExtendableRegexpCheck {
+public class StackOverflowErrorIgnoringRegexpCheck extends RegexpCheck {
 
-	
+	protected String getMessageBundle() {
+		// fix message bundle lookup
+		String className = RegexpCheck.class.getName();
+		return getMessageBundle(className);
+	}
+
+	//see AbstractViolationReporter
+	private static String getMessageBundle(String className) {
+		int endIndex = className.lastIndexOf('.');
+		if (endIndex < 0) {
+			return "messages";
+		}
+		String packageName = className.substring(0, endIndex);
+		return packageName + "." + "messages";
+	}
+
 	@Override
 	public void beginTree(DetailAST rootAST) {
 		try {
 			super.beginTree(rootAST);
 		} catch (StackOverflowError t) {
-			// ignore e.g. StackOverflowException, can happen on large files with some complex regexes
+			// ignore e.g. StackOverflowException, can happen on large files
+			// with some complex regexes
 		}
 	}
 
